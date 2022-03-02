8d6cfb7d43e496ccc0da448fc6d7b9e1c7cd8681
emrdagkusu <emrdagkusu@gmail.com>
2022-03-03 00:54:05 +0300

    add comments to mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -11,40 +11,50 @@
 
 // Put your code here.
 
+    // Take the value of register 0
     @R0
-    D = M
+    D=M
 
+    // Put 0 to register 2
     @R2
-    M = 0
+    M=0
 
+    // Take the value of register 1
     @R1
-    D = M
+    D=M
 
+    // Set an index and put the value of register 0 to the index
     @index
-    M = D
+    M=D
 
 (LOOP)
+    // Take the value of the index
     @index
-    D = M
+    D=M
 
+    // If index is 0, jump to the end
     @END
-    D ; JEQ
+    D;JEQ
 
+    // Take the value of register 0
     @R0
-    D = M
+    D=M
 
+    // Add the value of register 0 to the register 2 which will sum of the product
     @R2
-    D = D + M
-    M = D
+    D=D+M
+    M=D
 
+    // Decrease the index by 1
     @index
-    D = M
-    D = D - 1
-    M = D
+    D=M
+    D=D-1
+    M=D
 
+    // Continue the loop
     @LOOP
-    0; JMP
+    0;JMP
 
 (END)
     @END
-    0; JMP
+    0;JMP
